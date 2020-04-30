32fb5e83c7c629496b2380b80eeb0c799ff2dbca
Robert Manzke <robert.manzke@fh-kiel.de>
2020-04-30 10:21:39 +0200

    lv 1

--- main/blink.c
+++ main/blink.c
@@ -8,8 +8,10 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
@@ -18,39 +20,43 @@
 */
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
+TaskHandle_t myTaskHandle1 = NULL, myTaskHandle2 = NULL;
+QueueHandle_t myQueue = NULL;
+
 void myTask1(void * pvParameters )
 {
     int val = (int)pvParameters;
+    ESP_LOGE("Task1", "My value is %d, setting GPIOs for blink and starting endless loop", val);
     gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     while(1) {
-
-        printf("Turning off the LED Value: %d\n", val);
+        //printf("Turning off the LED Value: %d\n", val);
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-        printf("Turning on the LED\n");
+        //printf("Turning on the LED\n");
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+        int receivedVal;
+        xQueueReceive(myQueue, &receivedVal, portMAX_DELAY);
+        ESP_LOGW("Task1", "Received queue element with value %d", receivedVal);
     }
 }
 
 void myTask2(void * pvParameters )
 {
     int val = (int)pvParameters;
+    ESP_LOGW("Task2", "My value %d, starting endless loop", val);
     while(1) {
+        val++;
         printf("Hello from myTask2 Value: %d\n", val);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        xQueueSend(myQueue, &val, portMAX_DELAY);
     }
 }
 
 void app_main(void)
 {
-    TaskHandle_t myTaskHandle1 = NULL;
-    xTaskCreate(myTask1, "myTask", 4096, (void*)42, 10, &myTaskHandle1);
-    TaskHandle_t myTaskHandle2 = NULL;
-    xTaskCreate(myTask2, "myTask2", 4096, (void*)22, 10, &myTaskHandle2);
-    for(;;){
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+    ESP_LOGI("Main", "Ich bin gerade in der Main!");
+    myQueue = xQueueCreate(10, sizeof(int));
+    xTaskCreatePinnedToCore(myTask1, "myTask1", 4096, (void*)42, 10, &myTaskHandle1, 0);
+    xTaskCreatePinnedToCore(myTask2, "myTask2", 4096, (void*)22, 10, &myTaskHandle2, 1);
 }
